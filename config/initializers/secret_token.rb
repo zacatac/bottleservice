24dec1d793b449f6a2ba8c1ea1362d7e67b42a46
zacatac <field.zackery@gmail.com>
2014-04-12 01:45:56 -0700

    init rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bottleservice::Application.config.secret_key_base = '1c8909c63461503bdcac8e71848c710c16706df40c74267476dfe0c88e13c7268a21b66dea2de23206566eded16c8d340f5712add17065e78507b4f651dbde24'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bottleservice::Application.config.session_store :cookie_store, key: '_bottleservice_session'
